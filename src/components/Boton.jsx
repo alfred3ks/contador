3ef2765c754cca2e2d173b0c5b3d46e06059997a
Alfredo Sánchez <alfred3ks@gmail.com>
2023-03-24 18:40:39 +0100

    Retoques finales en movil del contador.

--- src/components/Boton.jsx
+++ src/components/Boton.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import style from "./Boton.module.css";
 
 const Boton = ({ texto, esBotonDeClic, manejarClic }) => {
--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,4 +1,3 @@
-import React, { useState } from "react";
 import style from "./Contador.module.css";
 
 const Contador = ({ numClics }) => {
